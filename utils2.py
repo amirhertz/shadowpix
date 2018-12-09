c8db6be23a488afd539d5e9eb54105da0e849cb9
amirhertz <amirhertz@mail.tau.ac.il>
2018-12-09 17:27:03 +0200

    second first commit

---
+++ utils2.py
@@ -0,0 +1,102 @@
+import numpy as np
+import trimesh
+
+
+def export_mesh(vertices_gropus, faces_groups, file):
+    with open(file, 'w+') as f:
+        for v_group in vertices_gropus:
+            v_group.shape = (v_group.size // 3, 3)
+            for v in v_group:
+                f.write("v %f %f %f\n" % (v[0], v[1], v[2]))
+        for f_group in faces_groups:
+            f_group.shape = (f_group.size // 3, 3)
+            f_group += 1
+            for face in f_group:
+                f.write("f %d %d %d\n" % (face[0], face[1], face[2]))
+    show_mesh(file)
+
+
+def show_mesh(file):
+    mesh = trimesh.load_mesh(file)
+    # for facet in mesh.facets:
+    #     mesh.visual.face_colors[facet] = trimesh.visual.random_color()
+    mesh.show()
+
+def export_local(r, u, v, wall_th, file_name):
+    rows, cols = u.shape
+    cols_helper = np.arange(0, cols, dtype=np.uint32)
+    row_helper = np.arange(0, rows, dtype=np.uint32)
+    r_vertices = np.zeros([rows, cols - 1, 4, 3])
+    u_vertices = np.zeros([rows, cols, 4, 3])
+    v_vertices = np.zeros([rows, cols - 1, 4, 3])
+
+    r_faces = np.zeros([rows, cols - 1, 2, 3], dtype=np.uint32)
+    u_faces = np.zeros([rows, cols, 2, 3], dtype=np.uint32)
+
+    # set z
+    r_vertices[:, :, :, 2] = r[:, :, np.newaxis]
+    u_vertices[:, :, :, 2] = u[:, :, np.newaxis]
+    v_vertices[:, :, :, 2] = v[:, :, np.newaxis]
+
+    # set x
+    r_vertices[:, :, 0, 0] = wall_th + cols_helper[np.newaxis, :-1] * (1 + wall_th)
+    r_vertices[:, :, 1, 0] = r_vertices[:, :, 0, 0]
+    r_vertices[:, :, 2, 0] = r_vertices[:, :, 0, 0] + 1
+    r_vertices[:, :, 3, 0] = r_vertices[:, :, 2, 0]
+
+    u_vertices[:, :, 0, 0] = cols_helper[np.newaxis, :] * (1 + wall_th)
+    u_vertices[:, :, 1, 0] = u_vertices[:, :, 0, 0]
+    u_vertices[:, :, 2, 0] = u_vertices[:, :, 0, 0] + wall_th
+    u_vertices[:, :, 3, 0] = u_vertices[:, :, 2, 0]
+
+    v_vertices[:, :, 0, 0] = r_vertices[:, :, 0, 0]
+    v_vertices[:, :, 1, 0] = r_vertices[:, :, 0, 0]
+    v_vertices[:, :, 2, 0] = r_vertices[:, :, 2, 0]
+    v_vertices[:, :, 3, 0] = r_vertices[:, :, 2, 0]
+
+    # set y
+    r_vertices[:, :, 0, 1] = wall_th + row_helper[:, np.newaxis] * (1 + wall_th)
+    r_vertices[:, :, 3, 1] = r_vertices[:, :, 0, 1]
+    r_vertices[:, :, 1, 1] = r_vertices[:, :, 0, 1] + 1
+    r_vertices[:, :, 2, 1] = r_vertices[:, :, 1, 1]
+
+    u_vertices[:, :, 0, 1] = wall_th + row_helper[:, np.newaxis] * (1 + wall_th)
+    u_vertices[:, :, 3, 1] = u_vertices[:, :, 0, 1]
+    u_vertices[:, :, 1, 1] = u_vertices[:, :, 0, 1] + 1
+    u_vertices[:, :, 2, 1] = u_vertices[:, :, 1, 1]
+
+    v_vertices[:, :, 0, 1] = row_helper[:, np.newaxis] * (1 + wall_th)
+    v_vertices[:, :, 3, 1] = v_vertices[:, :, 0, 1]
+    v_vertices[:, :, 1, 1] = v_vertices[:, :, 0, 1] + wall_th
+    v_vertices[:, :, 2, 1] = v_vertices[:, :, 1, 1]
+
+    # faces
+    fill = np.array([cols_helper[:-1] * 4, cols_helper[:-1] * 4 + 1, cols_helper[:-1] * 4 + 2])
+    r_faces[:, :, 0, :] = np.transpose(fill)[np.newaxis, :, :]
+    fill = np.array([cols_helper[:-1] * 4, cols_helper[:-1] * 4 + 2, cols_helper[:-1] * 4 + 3])
+    r_faces[:, :, 1, :] = np.transpose(fill)[np.newaxis, :, :]
+    r_faces += (row_helper * (cols - 1) * 4)[:, np.newaxis, np.newaxis, np.newaxis]
+
+    fill = np.array([cols_helper * 4, cols_helper * 4 + 1, cols_helper * 4 + 2])
+    u_faces[:, :, 0, :] = np.transpose(fill)[np.newaxis, :, :]
+    fill = np.array([cols_helper * 4, cols_helper * 4 + 2, cols_helper * 4 + 3])
+    u_faces[:, :, 1, :] = np.transpose(fill)[np.newaxis, :, :]
+    u_faces += (row_helper * cols * 4)[:, np.newaxis, np.newaxis, np.newaxis] + (v_vertices.size + r_vertices.size) // 3
+    v_faces = r_faces + r_vertices.size // 3
+    export_mesh([r_vertices, u_vertices, v_vertices], [r_faces, u_faces, v_faces], file_name)
+
+    print('done')
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # TESTS # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+if __name__ == '__main__':
+    col_s = 100
+    row_s = 100
+    r_a = np.random.rand(row_s, col_s)
+    u_a = np.random.rand(row_s, col_s + 1)
+    v_a = np.random.rand(row_s, col_s)
+    export_local(r_a, u_a, v_a, 0.3, './test.obj')
+    # show_mesh('/home/amir/projects/meshnet/datasets/vase/rp_500/train/0001.obj')
