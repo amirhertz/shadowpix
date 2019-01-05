8ea6265aaf75c9ca31c621cd67b5d767cf510681
amirhertz <amirhertz@mail.tau.ac.il>
2019-01-05 18:09:14 +0200

    update global

--- global_method_gpu.py
+++ global_method_gpu.py
@@ -9,7 +9,6 @@ import torch
 from torch import nn
 
 
-#
 class GlobalMethod:
     def __init__(self, images_paths, image_size, w_g, w_s, radius=20, device=torch.device('cuda:0')):
         self.num_images = len(images_paths)
@@ -21,32 +20,35 @@ class GlobalMethod:
         self.smooth_conv = self.init_smooth_conv(device)
         self.gp_images = self.grad_conv(self.smooth_conv(self.raw_images))
         self.size = image_size
-        self.height_field = torch.zeros([1, 1, image_size, image_size], requires_grad=False).to(device)
-        self.height_field_images = torch.ones([self.num_images,1, image_size, image_size], requires_grad=False).to(device)  # all white
+        self.heightfield = torch.zeros([1, 1, image_size, image_size], requires_grad=False).to(device)
+        self.heightfield_images = torch.ones([self.num_images,1, image_size, image_size], requires_grad=False).to(device)  # all white
         self.T = 0
         self.alpha = 0
         self.w_g = w_g
         self.w_s = w_s
         self.radius = radius
         self.device = device
-        self.value = self.objective(self.height_field_images)
+        self.value = self.objective(self.heightfield_images)
         self.pad = nn.ConstantPad1d((0, radius), -1000)
-        buffer_vector = torch.arange(1, self.radius +1).to(device)
-        selecting_matrix = torch.arange(0, self.size).unsqueeze(1).to(device)
+        buffer_vector = torch.arange(1, self.radius +1, requires_grad=False).to(device)
+        selecting_matrix = torch.arange(0, self.size, requires_grad=False).unsqueeze(1).to(device)
         self.selecting_matrix = selecting_matrix + buffer_vector
         self.buffer_vector = buffer_vector.float()
 
+        self.shadow_vec = torch.arange(1, self.radius + 2, dtype=torch.float, requires_grad=False).to(device)
+        self.shadow_vec_flipped = self.shadow_vec.float().flip(0)
+
     def calculate_update(self, row, col):
-        update_images = self.height_field_images.clone()
+        update_images = self.heightfield_images.clone()
         for i in range(self.num_images):
             if i == 0:
-                to_compare = self.height_field[0, 0, row, :]
+                to_compare = self.heightfield[0, 0, row, :]
             elif i == 1:
-                to_compare = self.height_field[0, 0, row, :].flip(0)
+                to_compare = self.heightfield[0, 0, row, :].flip(0)
             elif i == 2:
-                to_compare = self.height_field[0, 0, :, col]
+                to_compare = self.heightfield[0, 0, :, col]
             else:
-                to_compare = self.height_field[0, 0, :, col].flip(0)
+                to_compare = self.heightfield[0, 0, :, col].flip(0)
             height_select = self.pad(to_compare)
 
             compare_values = height_select[self.selecting_matrix] - self.buffer_vector
@@ -61,17 +63,15 @@ class GlobalMethod:
         return update_images
 
     def mse(self, x, y):
-        l = x - y
-        l = (l * l).sum()
-        l /= (self.size * self.size * self.num_images)
-        return l
-
-
+        loss = x - y
+        loss = (loss * loss).sum()
+        loss /= (self.size * self.size * self.num_images)
+        return loss
 
     def objective(self, height_field_images):
         height_field_image_smooth = self.smooth_conv(height_field_images)
-        height_field_gradient = self.grad_conv(self.height_field)
-        height_field_image_both = self.grad_conv(self.smooth_conv(self.height_field_images))
+        height_field_gradient = self.grad_conv(self.heightfield)
+        height_field_image_both = self.grad_conv(self.smooth_conv(self.heightfield_images))
         value = (1 - self.w_g - self.w_s) * self.mse(height_field_image_smooth, self.raw_images)
         if self.w_g:
             value += self.w_g * self.mse(height_field_image_both, self.gp_images)
@@ -91,14 +91,16 @@ class GlobalMethod:
                     x = x.cpu()
                 d[att] = x
         d_path = './data/%s.pkl' % name
+        print("checkpointing %s" % name)
         with open(d_path, 'wb') as f:
             pickle.dump(d, f, pickle.HIGHEST_PROTOCOL)
 
     def load_data(self, name):
         d_path = './data/%s.pkl' % name
         if not os.path.isfile(d_path):
-            print("data file is not exist")
+            print("data file- %s is not exist" % name)
             return
+        print("Loading %s" % name)
         with open(d_path, 'rb') as f:
             d = pickle.load(f)
             for att in d:
@@ -107,31 +109,90 @@ class GlobalMethod:
                     x = x.to(self.device)
                 self.__setattr__(att, x)
 
+    def is_step_valid_a(self, row, col):
+        new_value = self.heightfield[0, 0, row, col]
+        for i in range(len(self.raw_images)):
+            # check left
+            if i == 0 and col > self.radius:
+                window = self.heightfield[0, 0, row, col - self.radius - 1: col] + self.shadow_vec_flipped
+            # check right
+            elif i == 1 and col < self.size - self.radius - 1:
+                window = self.heightfield[0, 0, row, col + 1: col + self.radius + 2] + self.shadow_vec
+            # check down
+            elif i == 2 and row > self.radius:
+                window = self.heightfield[0, 0, row - self.radius - 1: row, col] + self.shadow_vec_flipped
+            # check up
+            elif row < self.size - self.radius - 1:
+                window = self.heightfield[0, 0, row + 1: row + self.radius + 2, col] + self.shadow_vec
+            else:
+                continue
+            if window.max() < new_value:
+                return False
+        return True
+
+    def is_step_valid_b(self, row, col):
+        for i in range(len(self.raw_images)):
+            # check left
+            if i == 0 and col > self.radius:
+                to_compare = self.heightfield[0, 0, row, col - self.radius - 1]
+                window = self.heightfield[0, 0, row, col - self.radius: col + 1] + self.shadow_vec
+            # check right
+            elif i == 1 and col < self.size - self.radius - 1:
+                to_compare = self.heightfield[0, 0, row, col + self.radius + 1]
+                window = self.heightfield[0, 0, row, col: col + self.radius + 1] + self.shadow_vec_flipped
+            # check down
+            elif i == 2 and row > self.radius:
+                to_compare = self.heightfield[0, 0, row - self.radius - 1, col]
+                window = self.heightfield[0, 0, row - self.radius: row + 1, col] + self.shadow_vec
+            # check up
+            elif row < self.size - self.radius - 1:
+                to_compare = self.heightfield[0, 0, row + self.radius + 1, col]
+                window = self.heightfield[0, 0, row: row + self.radius + 1, col] + self.shadow_vec_flipped
+            else:
+                continue
+            if window.max() < to_compare:
+                return False
+        return True
+
+    def is_step_valid(self, row, col, delta):
+        if delta:
+            return self.is_step_valid_a(row, col)
+        else:
+            return self.is_step_valid_b(row, col)
+
+    def export_mesh(self, name, light_angle):
+        s = 1 / np.tan(light_angle * np.pi / 180)
+        height_field = self.heightfield.cpu().squeeze(0).squeeze(0).numpy() * s
+        height_field -= height_field.min()
+        heightfield_to_mesh(height_field, './models/%s.obj' % name)
+
     def step(self):
         for _ in range(self.size * self.size):
             row = np.random.randint(0, self.size)
             col = np.random.randint(0, self.size)
-            delta = np.random.randint(-5, 6)
-            self.height_field[0, 0,row, col] += delta
-            if self.is_step_valid(row, col):
+            delta = 0
+            while not delta:
+                delta = np.random.randint(-5, 6)
+            self.heightfield[0, 0,row, col] += delta
+            if self.is_step_valid(row, col, delta > 0):
                 updated_images = self.calculate_update(row, col)
                 new_value = self.objective(updated_images)
-                profit = self.value - new_value
+                profit = (self.value - new_value) * 100
                 # if profit < 0:
                 #     print('prob is: ' + str(np.e**(profit / self.T )))
-                if profit > 0:  # or np.random.random() < np.e**(profit / self.T):
+                if profit > 0 or np.random.random() < np.e**(profit / self.T):
                     self.T -= self.alpha
-                    self.height_field_images = updated_images
+                    self.heightfield_images = updated_images
                     self.value = new_value
                     return new_value
                 else:
-                    self.height_field[0, 0, row, col] -= delta
+                    self.heightfield[0, 0, row, col] -= delta
             else:
-                self.height_field[0, 0, row, col] -= delta
+                self.heightfield[0, 0, row, col] -= delta
         return -1
 
     def optimize(self, steps, name):
-        self.T = 2
+        self.T = 0.01
         min_t = 0
         failed_counter = 0
         self.alpha = (self.T - min_t) / steps
@@ -147,47 +208,13 @@ class GlobalMethod:
 
             if (i + 1) % 2000 == 0:
                 self.export_data(name)
-                print("checkpointing")
-            if (i) % 1000 == 0:
+            if i % 500 == 0:
                 if value > 0:
                     print("Objective value after %d steps is %.3f" % (i +1, value))
                 else:
                     print("step %d failed" % i)
         print('done')
         self.export_data(name)
-
-    def is_step_valid(self, row, col):
-        return True
-        # new_value = self.height_field[row, col]
-        # for i in range(len(self.raw_images)):
-        #     check_col = None
-        #     window = None
-        #     # check left
-        #     if i == 0 and col > self.radius:
-        #         check_col = self.height_field[row, col - self.radius - 1]
-        #         window = self.height_field[row, col - self.radius: col] - self.buffer_vector
-        #     # check right
-        #     elif i == 1 and col < self.size - self.radius - 1:
-        #         check_col = self.height_field[row, col + self.radius + 1]
-        #         window = self.height_field[row, col: col +  self.radius] - np.flip(self.buffer_vector, 0)
-        #     # check down
-        #     elif i == 2 and row > self.radius:
-        #         check_col = self.height_field[row - self.radius - 1, col]
-        #         window = self.height_field[row - self.radius: row, col] - self.buffer_vector
-        #     # check up
-        #     elif row < self.size - self.radius - 1:
-        #         check_col = self.height_field[row + self.radius + 1, col]
-        #         window = self.height_field[row: row + self.radius, col] - np.flip(self.buffer_vector, 0)
-        #     if check_col and new_value - self.radius > check_col:
-        #         if np.max(window) < check_col:
-        #             return False
-        # return True
-
-    def export_mesh(self, name, light_angle):
-        s = 1 / np.tan(light_angle * np.pi / 180)
-        height_field = self.height_field.cpu().squeeze(0).squeeze(0).numpy() * s
-        height_field -= height_field.min()
-        heightfield_to_mesh(height_field, './models/%s.obj' % name)
 
     @staticmethod
     def init_grad_conv(device):
@@ -200,10 +227,12 @@ class GlobalMethod:
             conv_y.weight = nn.Parameter(filter_y.unsqueeze(0).unsqueeze(0))
         conv_x = conv_x.to(device)
         conv_y = conv_y.to(device)
+
         def apply_gradient_filter(images):
             gradient_x = conv_x(images)
             gradient_y = conv_y(images)
             g = torch.sqrt(torch.pow(gradient_x, 2) + torch.pow(gradient_y, 2))
+            g = torch.clamp(g, 0, 1)
             return g
 
         return apply_gradient_filter
@@ -215,24 +244,24 @@ class GlobalMethod:
         with torch.no_grad():
             conv.weight = nn.Parameter(filter.unsqueeze(0).unsqueeze(0))
         conv = conv.to(device)
+
         def apply_smooth_filter(images):
-            return conv(images)
+            return torch.clamp(conv(images), 0, 1)
         return apply_smooth_filter
 
 
 def global_method():
-    path_a = './images/magritte.jpg'
-    path_b = './images/monet.jpeg'
-    path_c = './images/roy.jpg'
-    path_d = './images/miro.jpg'
-    train_name = 'global_paintings_gpu'
-
+    path_a = './images/Yoda.jpg'
+    path_b = './images/Darth-Vader.jpg'
+    path_c = './images/Stormtrooper.jpg'
+    path_d = './images/Ewok.jpg'
+    train_name = 'global_starwars_ann_gpu'
     paths = [path_a, path_b, path_c, path_d]
-    GBM = GlobalMethod(paths, 400, 0.05, 0, radius=40, device=torch.device('cuda:0'))
-    # GBM.export_data(train_name)
-    # GBM.optimize(1000000, train_name)
-    GBM.load_data(train_name)
-    GBM.export_mesh(train_name, 60)
+    gbm = GlobalMethod(paths, 400, 0.1, 0, radius=40, device=torch.device('cuda:1'))
+    # gbm.load_data(train_name)
+    gbm.optimize(9000000, train_name)
+    # gbm.load_data(train_name)
+    # gbm.export_mesh(train_name, 60)
 
 
 if __name__ == '__main__':
